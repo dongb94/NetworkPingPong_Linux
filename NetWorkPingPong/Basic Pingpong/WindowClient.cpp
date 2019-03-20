4c86ec23a530922eef6a28c9cb947725fd5ef6bb
DONGGEON BYEON <dongb94@gmail.com>
2019-03-20 18:33:51 +0900

    Update window client

--- NetWorkPingPong/Basic Pingpong/WindowClient.cpp
+++ NetWorkPingPong/Basic Pingpong/WindowClient.cpp
@@ -4,18 +4,28 @@
 #pragma comment(lib, "ws2_32.lib")
 
 int main() {
-	SOCKET ConnectSocket = INVALID_SOCKET;
+	//WSADATA       wsaData;                                                     //WinSock을 위한 내부 자료구조
+
+	//WORD            wVersionRequested = MAKEWORD(2, 2);
+
+	//WSAStartup(wVersionRequested, &wsaData);
+
+	SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	struct addrinfo *ptr = NULL;
 	struct sockaddr_in addr;
 
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(80);
-	addr.sin_addr.S_un.S_addr = inet_addr("221.144.2.165");
+	//addr.sin_addr.S_un.S_addr = inet_addr("221.144.2.165");
+	addr.sin_addr.S_un.S_addr = inet_addr("8.8.8.8");
 
 	char sendBuffer[] = "Test Message";
 	char recvbuf[256];
 
-	connect(ConnectSocket, (sockaddr*)&addr, sizeof(sockaddr_in));
+	if (connect(ConnectSocket, (sockaddr*)&addr, sizeof(sockaddr_in)) == -1) {
+		printf("Error No = %d\n", errno);
+		fprintf(stderr, "C Error : %s\n", strerror(errno));
+	}
 
 	printf("%s\n", sendBuffer);
 
@@ -24,6 +34,7 @@ int main() {
 			continue;
 		}
 		else {
+			printf("Error No = %d\n", errno);
 			fprintf(stderr, "Send Error : %s\n", strerror(errno));
 			return -1;
 		}
